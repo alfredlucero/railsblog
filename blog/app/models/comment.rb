4a6f2b4bea4902e091cc774e7d89c9d1dddfa62d
alfredlucero <alfred.lucero@ucla.edu>
2022-09-06 14:41:51 -0700

    Added first concern for visibility

--- blog/app/models/comment.rb
+++ blog/app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
+  include Visible
+
   # Active Record association with :references keyword in shell command - article_id foreign key
   # Each comment belongs to one article; one article can have many comments
   belongs_to :article
