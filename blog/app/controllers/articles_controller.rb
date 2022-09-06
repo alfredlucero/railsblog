68ec4a36d8081a6ed734d00354aee9567286e6a8
alfredlucero <alfred.lucero@ucla.edu>
2022-09-06 11:12:40 -0700

    Added create article flow

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -22,4 +22,34 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  # Instantiates a new article but does not save it; this article will be used in the view when building the form
+  # By default, the new action will render articles/new.html.erb
+  # GET /articles/new is mapped to this action; it doesn't attempt to save @article
+  def new
+    @article = Article.new
+  end
+
+  # When we submit the new form, the POST /articles request is mapped to this create action
+  # It does attempt to save @article and validations are checked
+  def create
+    @article = Article.new(article_params)
+    
+    # If saved successfully, action redirects browser to article's page at http://localhost:3000/articles/#{@article.id}
+    if @article.save
+      # redirect_to causes browser to make a new request; improtant to use this after mutating database or application state
+      # otherwise if user refreshes the page, the browser will make the same request and mutation will be repeated
+      redirect_to @article
+    # Otherwise, action redisplays form by rendering new.html.erb with status code 422 with the error messages
+    else
+      # Render = render specified view for current request
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    # Using strong parameters to extract out form field paramaters
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,5 +1,9 @@
 # Model is a Ruby class used to represent data
 # Models can interact with application's database through a feature of Rails called Active Record
 # bin/rails generate model Article title:string body:text
+# Active Record automatically defines model attributes for every table column so you don't have to declare those attributes in your model file
 class Article < ApplicationRecord
+  # Add validations to help with invalid user input; if any of the checks fail, the save will be aborted and appropriate error messages will be added to the errors attribute of the model object
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end
